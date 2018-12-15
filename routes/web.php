c2261c3015e3e7a5fc47a6c05190f2d43531a01d
Marta <coscu.000@gmail.com>
2018-12-15 11:10:45 +0100

    fix home

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('users', 'userController');
 
